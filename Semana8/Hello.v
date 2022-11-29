b443b86fd0f06748f398235062738c78b1c3b555
shell0 <marcello.menjivar@gmail.com>
2022-11-29 12:43:39 -0600

    Agregando programas en Verilog

---
+++ Semana8/Hello.v
@@ -0,0 +1,10 @@
+// Hello World en Verilog
+
+module hello;
+  initial
+    begin
+      $display("Hello, World");
+      $display("I am an Icarus Verilog Program.");
+      $finish ;
+    end
+endmodule
